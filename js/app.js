d766794bcbf39fcca367b67fae4b0efd7a88af43
https-allan <code.allanjose@gmail.com>
2024-07-03 21:54:20 -0400

    Refatoração do código

--- js/app.js
+++ js/app.js
@@ -1,30 +1,34 @@
 const key = "cebcd482eda57fa9a6714c1c2ba91885";
 
-function addToScreen(dados) {
-  document.querySelector(".city_name").innerHTML = `Tempo em ${dados.name}`;
+function updateWeatherDisplay(data) {
+  document.querySelector(".city_name").innerHTML = `Tempo em ${data.name}`;
   document.querySelector(".temperature").innerHTML = `${Math.floor(
-    dados.main.temp
+    data.main.temp
   )}°C`;
   document.querySelector(
     ".forecast"
-  ).innerHTML = `${dados.weather[0].description}`;
-  document.querySelector(".moisture").innerHTML = `Umidade: ${dados.main.humidity}%`
-  document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+  ).innerHTML = `${data.weather[0].description}`;
+  document.querySelector(
+    ".moisture"
+  ).innerHTML = `Umidade: ${data.main.humidity}%`;
+  document.querySelector(
+    ".icon"
+  ).src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
 }
 
-async function searchCityApi(city) {
-  const api = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
-  );
-  const response = await api.json();
-
-  console.log(response);
-
-  addToScreen(response);
+async function fetchWeatherData(city) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
+    );
+    const data = await response.json();
+    updateWeatherDisplay(data);
+  } catch (error) {
+    console.error("Erro ao buscar dados da API:", error);
+  }
 }
 
 function searchCity() {
   const city = document.querySelector(".input_city").value;
-
-  searchCityApi(city);
+  fetchWeatherData(city);
 }
